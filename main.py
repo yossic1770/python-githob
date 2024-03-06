f7e2fae9b575f6497953b075572d08dd1f92679e
yossic770 <yossic770@gmail.com>
2024-03-06 19:20:34 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('PyCharm')
+
+    print("im thecho "# python-githob" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/yossic1770/python-githob.gite one")
